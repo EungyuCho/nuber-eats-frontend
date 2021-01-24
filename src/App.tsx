9507528ebcfda91439fed8f222aa8225c7baf9cf
gyu <cho2304@naver.com>
2021-01-24 19:29:04 +0900

    :heavy_plus_sign: apolloClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-
 function App() {
-  return (
-      <div className="bg-black">
-          <h1 className="text-6xl text-yellow-50">hello tailwind</h1>
-      </div>
-  );
+    return (
+        <div className="bg-black">
+            <h1 className="text-6xl text-yellow-50">hello tailwind</h1>
+        </div>
+    );
 }
 
 export default App;
