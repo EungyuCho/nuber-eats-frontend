2a7ec08e1c7ca9b6e92850a99caf0cd08332fd5c
gyu <cho2304@naver.com>
2021-01-24 21:18:44 +0900

    :zap: apollo local field

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,11 @@
-import React from 'react';
+import React from "react";
+import { isLoggedInVar } from "../apollo";
 
-export const LoggedInRouter = () => <span>Logged In</span>;
+export const LoggedInRouter = () => {
+  return (
+    <>
+      <h1>Logged In</h1>
+      <button onClick={() => isLoggedInVar(false)}>Log Out</button>
+    </>
+  );
+};
