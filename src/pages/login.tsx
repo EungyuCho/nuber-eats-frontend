082a3ef169f3dfa63f447599d9472bc59a6f4f33
gyu <cho2304@naver.com>
2021-01-25 17:56:23 +0900

    :heavy_plus_sign: apollo(codegen)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,73 @@
 import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import { useForm } from "react-hook-form";
+import { FormError } from "../components/form-error";
+import {
+  loginMutation,
+  loginMutationVariables,
+} from "../__generated__/loginMutation";
+
+const LOGIN_MUTATION = gql`
+  mutation loginMutation($email: String!, $password: String!) {
+    login(input: { email: $email, password: $password }) {
+      ok
+      token
+      error
+    }
+  }
+`;
+interface ILoginForm {
+  email: string;
+  password: string;
+}
 
 export const Login = () => {
+  const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
+  const [loginMutation, { data }] = useMutation<
+    loginMutation,
+    loginMutationVariables
+  >(LOGIN_MUTATION);
+  const onSubmit = () => {
+    const { email, password } = getValues();
+    loginMutation({
+      variables: {
+        email,
+        password,
+      },
+    });
+  };
+  console.log(data?.login);
+
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
       <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
         <h3 className="text-3xl text-gray-800">Log In</h3>
-        <form className="flex flex-col mt-5 px-5">
+        <form
+          className="flex flex-col mt-5 px-5"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <input
+            ref={register({ required: "Email is required" })}
+            required
+            name="email"
+            type="email"
             placeholder="Email"
             className="bg-gray-300 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
           />
+          {errors.email?.message && (
+            <FormError errorMessage={errors.email?.message} />
+          )}
           <input
+            ref={register({ required: "password is required" })}
+            required
+            name="password"
+            type="password"
             placeholder="Password"
             className="bg-gray-300 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 py-3 px-5 rounded-lg"
           />
+          {errors.password?.message && (
+            <FormError errorMessage={errors.password?.message} />
+          )}
           <button className="bg-gray-800 py-3 px-5 text-white font-medium text-lg mt-3 rounded-lg focus:outline-none hover:opacity-90">
             Log In
           </button>
