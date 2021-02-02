a769f5578bf2c0a4a818ceaafd675daa67d690a4
gyu <cho2304@naver.com>
2021-02-02 21:55:28 +0900

    :zap: Category Style

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -51,5 +51,32 @@ export const Restaurants = () => {
 
   console.log(data);
 
-  return <h1>Restaurants</h1>;
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input
+          type="Search"
+          className="input rounded-md w-3/12 border-0"
+          placeholder="Search restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {data?.allCategories.categories?.map((category) => (
+              <div className="text-center">
+                <div
+                  className="w-14 h-14 rounded-full bg-cover hover:bg-gray-100 cursor-pointer"
+                  style={{ backgroundImage: `url(${category.coverImg})` }}
+                ></div>
+                <span className="mt-1 text-sm font-semibold">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
